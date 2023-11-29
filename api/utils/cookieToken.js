f7af5d1efcc401b491cc9a4ef883cc3cfdf5cb14
Ngo Huynh Cong <congngo318@gmail.com>
2023-11-30 06:58:49 +0700

    update

---
+++ api/utils/cookieToken.js
@@ -0,0 +1,19 @@
+const cookieToken = (user, res) => {
+  const token = user.getJwtToken();
+
+  const options = {
+    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    httpOnly: true, // makes the token available only to backend
+    secure: true, // Only send over HTTPS
+    sameSite: "none", // Allow cross-origin requests
+  };
+
+  user.password = undefined;
+  res.status(200).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+
+module.exports = cookieToken;
